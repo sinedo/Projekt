d821252aeb969dd4107ed8eb2e7fecc790f76a2e
N1ceD0g <the100500@gmail.com>
2021-04-06 19:08:31 +0200

    xd

---
+++ Medinf/sites/search.php
@@ -0,0 +1,28 @@
+<head>
+<div class="topnav">
+  <input type="text" placeholder="Search...">
+</div>
+<head>
+
+<body>
+<table>
+  <tr>
+    <th>ID</th>
+    <th>Vorname</th>
+    <th>Nachname</th>
+    <th>Alter</th>
+  </tr>
+  <tr>
+    <td>123</td>
+    <td>XXXX</td>
+    <td>YYYY</td>
+    <td>ZZ</td>
+  </tr>
+  <tr>
+    <td>123</td>
+    <td>XXXX</td>
+    <td>YYYY</td>
+    <td>ZZ</td>
+  </tr>
+</table>
+<body>
