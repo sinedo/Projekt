3db456b669eba9fbb9d141b35b6cdf3658c4a688
sinedo <doniberisha32@gmail.com>
2021-04-08 23:11:24 +0200

    update: zeas.exe

--- Medinf/logic/login.php
+++ Medinf/logic/login.php
@@ -10,7 +10,7 @@ include '../includes/autoloader.inc.php'
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Verification</title>
-    <link rel="stylesheet" href="../styles/bulma.min.css" />
+    <link rel="stylesheet" href="../styles/bulma.css" />
     <link rel="stylesheet" type="text/css" href="../styles/theme.css">
 </head>
 <body>
@@ -29,13 +29,12 @@ include '../includes/autoloader.inc.php'
         
     ?>
 
-<section class="hero is-success is-fullheight">
-        <div class="hero-body">
-            <div class="container has-text-centered has-text-black">
-                <h2 class="Title">zeas..</h2>
-            </div>
-        </div>
-    </div>
-        
+    <section class="hero is-success is-fullheight">
+            <div class="hero-body">
+                <div class="container has-text-centered has-text-black">
+                    <h2 class="title is-1 has-text-black">zeas...</h2>
+                </div>
+            </div>  
+    </section>
 </body>
 </html>
