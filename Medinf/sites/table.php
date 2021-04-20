33ec0cd2efc3926b596753463f9a284a3323aba8
N1ceD0g <the100500@gmail.com>
2021-04-20 20:18:52 +0200

    hallo

--- Medinf/sites/table.php
+++ Medinf/sites/table.php
@@ -1,34 +1,89 @@
-<head>
-<div class="topnav">
-  <input type="text" placeholder="Search...">
-</div>
-<style>
-table, th, td {
-  border: 1px solid black;
-}
-</style>
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+
 
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>owo</title>
+
+    <link rel="stylesheet" href="../styles/bulma.css" />
+    <link rel="stylesheet" type="text/css" href="../styles/theme.css">
+</head>
+
 
 <body>
-<table>
-  <tr>
-    <th>ID</th>
-    <th>Vorname</th>
-    <th>Nachname</th>
-    <th>Alter</th>
-  </tr>
-  <tr>
-    <td>123</td>
-    <td>XXXX</td>
-    <td>YYYY</td>
-    <td>ZZ</td>
-  </tr>
-  <tr>
-    <td>123</td>
-    <td>XXXX</td>
-    <td>YYYY</td>
-    <td>ZZ</td>
-  </tr>
-</table>
+<section class="hero is-success is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                       
+                           <form class="box">
+                              <div class="field">
+                              <h1 class="title has-text-black">Search for Patients</h1>
+                              <div class="columns">
+                              <div class="column is-11">
+                                <input class="input" type="text" placeholder="Search..">
+                                </div>
+                                <div class="column">
+                                <button class="button is-link">GO</button>
+                                </div>
+
+                                </form>
+                                </div>  
+                                  <table class="table is-fullwidth">
+                                  <thead>
+                                    <tr>
+                                      <th> SVNR </th>
+                                      <th>Vorname</th>
+                                      <th>Nachname</th>
+                                      <th>MORE INFO</th>
+                                      </tr>
+                                      </thead>
+                                    
+                                      
+
+                                               <tbody>
+                                                <tr>
+                                                <td>1312</td>
+                                                <td>Eddobis</td>
+                                                <td>Mobile</td>
+                                                <td>is very huge :v</td>
+                                                </tr>
+                                            
+                                                <tr>
+                                                <td>1323</td>
+                                                <td>Clementine</td>
+                                                <td>Graph</td>
+                                                <td>lot of brain cells</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>2342</td>
+                                                <td>Kosdor</td>
+                                                <td>Fisch</td>
+                                                <td>rages at chess</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>6424</td>
+                                                <td>Daten</td>
+                                                <td>Bank</td>
+                                                <td>Eintrag</td>
+                                                </tr>
+                                              </tbody>
+                                              </table>
+                         </div>
+                    </div>
+               </div>
+            </div>
+        </div>
+    </section>
 <body>
+
+</html>
