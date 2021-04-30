ecc8d92b38f53b560ee3d70845525b7284c0fc3b
N1ceD0g <the100500@gmail.com>
2021-04-30 21:36:40 +0200

    fertig tabel

--- Medinf/sites/table.php
+++ Medinf/sites/table.php
@@ -11,7 +11,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>owo</title>
+<title>Patientensuche</title>
 
     <link rel="stylesheet" href="../styles/bulma.css" />
     <link rel="stylesheet" type="text/css" href="../styles/theme.css">
@@ -21,14 +21,14 @@ session_start();
 <body>
 <section class="hero is-success is-fullheight">
         <div class="hero-body">
-            <div class="container has-text-centered">
+            <div class="container">
                        
                            <form class="box">
                               <div class="field">
-                              <h1 class="title has-text-black">Search for Patients</h1>
+                              <h1 class="title has-text-black">Patientensuche</h1>
                               <div class="columns">
                               <div class="column is-11">
-                                <input class="input" type="text" placeholder="Search..">
+                                <input class="input" type="text" placeholder="Search for Patients...">
                                 </div>
                                 <div class="column">
                                 <button class="button is-link">GO</button>
@@ -63,6 +63,179 @@ session_start();
                                                 <td>lot of brain cells</td>
                                                 </tr>
 
+                                                <tr>
+                                                <td>2342</td>
+                                                <td>Kosdor</td>
+                                                <td>Fisch</td>
+                                                <td>rages at chess</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>6424</td>
+                                                <td>Daten</td>
+                                                <td>Bank</td>
+                                                <td>Eintrag</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>1312</td>
+                                                <td>Eddobis</td>
+                                                <td>Mobile</td>
+                                                <td>is very huge :v</td>
+                                                </tr>
+                                            
+                                                <tr>
+                                                <td>1323</td>
+                                                <td>Clementine</td>
+                                                <td>Graph</td>
+                                                <td>lot of brain cells</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>2342</td>
+                                                <td>Kosdor</td>
+                                                <td>Fisch</td>
+                                                <td>rages at chess</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>6424</td>
+                                                <td>Daten</td>
+                                                <td>Bank</td>
+                                                <td>Eintrag</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>1312</td>
+                                                <td>Eddobis</td>
+                                                <td>Mobile</td>
+                                                <td>is very huge :v</td>
+                                                </tr>
+                                            
+                                                <tr>
+                                                <td>1323</td>
+                                                <td>Clementine</td>
+                                                <td>Graph</td>
+                                                <td>lot of brain cells</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>2342</td>
+                                                <td>Kosdor</td>
+                                                <td>Fisch</td>
+                                                <td>rages at chess</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>6424</td>
+                                                <td>Daten</td>
+                                                <td>Bank</td>
+                                                <td>Eintrag</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>1312</td>
+                                                <td>Eddobis</td>
+                                                <td>Mobile</td>
+                                                <td>is very huge :v</td>
+                                                </tr>
+                                            
+                                                <tr>
+                                                <td>1323</td>
+                                                <td>Clementine</td>
+                                                <td>Graph</td>
+                                                <td>lot of brain cells</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>2342</td>
+                                                <td>Kosdor</td>
+                                                <td>Fisch</td>
+                                                <td>rages at chess</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>6424</td>
+                                                <td>Daten</td>
+                                                <td>Bank</td>
+                                                <td>Eintrag</td>
+
+                                                
+                                                <tr>
+                                                <td>2342</td>
+                                                <td>Kosdor</td>
+                                                <td>Fisch</td>
+                                                <td>rages at chess</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>6424</td>
+                                                <td>Daten</td>
+                                                <td>Bank</td>
+                                                <td>Eintrag</td>
+
+
+                                                
+                                                <tr>
+                                                <td>2342</td>
+                                                <td>Kosdor</td>
+                                                <td>Fisch</td>
+                                                <td>rages at chess</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>6424</td>
+                                                <td>Daten</td>
+                                                <td>Bank</td>
+                                                <td>Eintrag</td>
+
+
+                                                
+                                                <tr>
+                                                <td>2342</td>
+                                                <td>Kosdor</td>
+                                                <td>Fisch</td>
+                                                <td>rages at chess</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>6424</td>
+                                                <td>Daten</td>
+                                                <td>Bank</td>
+                                                <td>Eintrag</td>
+
+
+                                                
+                                                <tr>
+                                                <td>2342</td>
+                                                <td>Kosdor</td>
+                                                <td>Fisch</td>
+                                                <td>rages at chess</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>6424</td>
+                                                <td>Daten</td>
+                                                <td>Bank</td>
+                                                <td>Eintrag</td>
+
+
+                                                
+                                                <tr>
+                                                <td>2342</td>
+                                                <td>Kosdor</td>
+                                                <td>Fisch</td>
+                                                <td>rages at chess</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>6424</td>
+                                                <td>Daten</td>
+                                                <td>Bank</td>
+                                                <td>Eintrag</td>
+
+
+                                                
                                                 <tr>
                                                 <td>2342</td>
                                                 <td>Kosdor</td>
