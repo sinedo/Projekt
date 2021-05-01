3f9b831251811beeefa23c82fbb68c01b9f09471
sinedo <46466615+sinedo@users.noreply.github.com>
2021-05-01 22:07:33 +0200

    Update patient.class.php

--- Medinf/class/patient.class.php
+++ Medinf/class/patient.class.php
@@ -13,36 +13,6 @@ class Patient extends Database {
         $this->close($pdo);
         return $result;
     }
-
-    public function editpatient ($svn) {
-        $pdo = $this->connect();
-        $sql = "SELECT * FROM Patients WHERE svn LIKE $svn";
-        $result = $pdo->prepare($sql);
-        $result->execute();
-        $result->fetch();
-
-        
-    }
-    public function addnewpatient ($name, $surname, $birthday, $svn, $adress, $city, $post_code, $sex, $pronoun, $mobilephone) {
-        $pdo = $this->connect();
-        $sql = "SELECT svn FROM Patients WHERE svn LIKE $svn";
-        $result = $pdo->prepare($sql);
-        $result->execute();
-        $result->fetch();
-        if ($result = $svn) {
-            echo 'Dieser Patient existiert bereits!';
-           // editpatient($svn);
-        }
-        else {
-            $sql = "INSERT INTO patients (name, surname, svn, birthdate, sex, pronoun, address, city, post_code, mobilephone)
-            VALUES ('$name', '$surname','$svn', '$birthday', '$sex', '$pronoun', '$adress', '$city', '$post_code', '$mobilephone')";
-            $result = $pdo->prepare($sql);
-
-            $result->execute();
-            $this->close($pdo);
-            $result->fetch();
-        }
-    }
 }
 
 ?>
