f533e324b3ad6550ee0bbb360a02cf6f3aaaf515
N1ceD0g <the100500@gmail.com>
2021-05-23 13:41:58 +0200

    sers

---
+++ Medinf/sites/vitals.php
@@ -0,0 +1,97 @@
+<?php
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Vitalwerte</title>
+
+    <link rel="stylesheet" href="../styles/bulma.css" />
+    <link rel="stylesheet" type="text/css" href="../styles/theme.css">
+
+</head>
+
+
+<body>
+    <section class="hero is-success is-fullheight">
+        <div class="hero-head">
+            <div class="container">     
+
+                    <div class="box">
+                        <div class="columns">
+                            <div class="column is-half">
+                                    <h1 class="title has-text-black">Anrede: Herr</h1>
+                                    <h1 class="title has-text-black">Name: Edonis Berisha</h1>
+                                    <h1 class="title has-text-black">Geschlecht: M </h1>
+                                    <h1 class="title has-text-black">Pronomen: he/him</h1>
+                                </div>
+                                <div class="column">
+                                    <h1 class="title has-text-black">Alter: 22</h1>
+                                    <h1 class="title has-text-black">Größe: 1.90 cm</h1>
+                                    <h1 class="title has-text-black">Gewicht: 80 kg</h1>
+                                    <h1 class="title has-text-black">Aufenthaltsgrund: schwere Atemnot ICPD co.</h1>
+                                </div>  
+                            </div>    
+                        </div>
+                            <div class="box">
+                                <div class="columns">
+                                        <div class="dropdown is-active">
+                                            <div class="dropdown-trigger">  
+                                                </div>  
+                                                    <table class="table is-fullwidth">
+                                                    <thead>
+                                                    <tr>
+                                                    <th> <h1 class="title has-text-grey"> VITALWERT </h1> </th>
+                                                    <th> -></th>
+                                                    <th></th> 
+                                                </div>  
+                                            </div> 
+                                        </div>    
+                                </div>   
+                                    <div class="columns">
+                                        </form>
+                                        </div>  
+                                            <table class="table is-fullwidth">
+                                            <thead>
+                                            <tr>
+                                            <th> <h1 class="title has-text-grey"> VITALWERT </h1> </th>
+                                            <th> -></th>
+                                            <th></th> 
+                                        </div>    
+                                    </div> 
+                                        <div class="columns">
+                                            </form>
+                                            </div>  
+                                                <table class="table is-fullwidth">
+                                                <thead>
+                                                <tr>
+                                                <th> <h1 class="title has-text-grey"> VITALWERT </h1> </th>
+                                                <th> -></th>
+                                                <th></th> 
+                                            </div>    
+                                        </div> 
+                                            <div class="columns">
+                                                </form>
+                                                </div>  
+                                                    <table class="table is-fullwidth">
+                                                    <thead>
+                                                    <tr>
+                                                    <th> <h1 class="title has-text-grey"> VITALWERT </h1> </th>
+                                                    <th> -></th>
+                                                    <th></th> 
+                                                </div>    
+                                            </div>     
+                            </div>
+                        </div>
+                    </div>         
+            </div>  
+    </section>
+<body>                   
